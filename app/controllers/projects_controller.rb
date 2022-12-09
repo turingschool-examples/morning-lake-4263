5e760126307c5868f2c5b9fe4be41bc13f618c77
Samuel Cox <samc1253@gmail.com>
2022-12-09 14:25:02 -0700

    Add average contestant experience to project show page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,5 +2,6 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:project_id])
     @contestant_count = @project.contestant_count
+    @average_experience = @project.average_contestant_experience
   end
 end
