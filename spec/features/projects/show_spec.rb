ac5f31a6b0a4b946c4691dfdaa6ef5b142e92129
Braxton Eddings <beddings81@gmail.com>
2022-12-09 15:03:50 -0600

    Finish user story 1

---
+++ spec/features/projects/show_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'projects show page' do
+  it 'contains a projects name and material and the theme of the challenge' do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+    visit "/projects/#{news_chic.id}"
+
+    expect(page).to have_content(news_chic.name)
+    expect(page).to have_content(news_chic.material)
+    expect(page).to have_content(recycled_material_challenge.theme)
+  end
+end
