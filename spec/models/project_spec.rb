8b0defc1f14f463d44e5817a9ce1c21286c049fa
Alex Pitzel <pitzelalex@gmail.com>
2022-12-09 16:52:33 -0500

    Refactor: Move contestant count logic into project model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,22 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe '#count_contestants' do
+    it 'returns the number of contestants a project has' do
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: 'Gretchen Jones', age: 36, hometown: "NYC", years_of_experience: 12)
+
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+
+      expect(news_chic.count_contestants).to eq(1)
+
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+
+      expect(news_chic.count_contestants).to eq(2)
+    end
+  end
 end
