0e4ac26cabe26a261ab813e6aac220dcd9d6d763
Alex Pitzel <pitzelalex@gmail.com>
2022-12-09 16:31:49 -0500

    feat: display all project names associated with a contestant

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -26,20 +26,25 @@ RSpec.describe 'the contestants index page' do
     expect(page).to have_content(erin.name)
   end
 
-  it "under each name I see a list of project names they're working on"
-  # recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-  # furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+  it "under each name I see a list of project names they're working on" do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+  
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+    upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+  
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+  
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
 
-  # news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-  # boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
-  # upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
-
-  # gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
-  # kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
-
-  # ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
-  # ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
-  # ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
-  # ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
-
+    visit '/contestants'
+    
+    expect(page).to have_content('Projects: Boardfit, Upholstery Tuxedo')
+    expect(page).to have_content('Projects: News Chic, Upholstery Tuxedo')
+  end
 end
