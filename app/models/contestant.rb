38d0530a768ef7308fa9b6142abafbd3e2a16ad3
Kerynn Davis <kerynn.davis@gmail.com>
2022-12-10 18:42:24 -0800

    Add average years exp method to contestant model

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -2,4 +2,8 @@ class Contestant < ApplicationRecord
   validates_presence_of :name, :age, :hometown, :years_of_experience
   has_many :contestant_projects
   has_many :projects, through: :contestant_projects
+
+  def self.average_yrs_experience
+    Contestant.all.average(:years_of_experience)
+  end 
 end
--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe Contestant, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:projects).through(:contestant_projects)}
   end
+
+  describe 'average_yrs_experience' do 
+    it 'calclulates the average years of experience of all the contestants' do 
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+      erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+      contestants = Contestant.all
+      expect(contestants.average_yrs_experience).to eq(12)
+    end 
+  end 
 end
