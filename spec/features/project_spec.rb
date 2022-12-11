30d55bf2c0018df22510db37c908e397f6b336e8
Kerynn Davis <kerynn.davis@gmail.com>
2022-12-10 18:47:29 -0800

    complete user story ext 1

--- spec/features/project_spec.rb
+++ spec/features/project_spec.rb
@@ -33,5 +33,23 @@ RSpec.describe "project show page" do
 
       expect(page).to have_content("Number of Contestants: 3")
     end 
+
+    it 'shows the average years of experience for the contestants that worked on the project' do 
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: news_chic.id)
+
+      visit "/projects/#{news_chic.id}"
+
+      expect(page).to have_content("Average Contestant Experience: 11.0")
+    end 
   end 
 end 
