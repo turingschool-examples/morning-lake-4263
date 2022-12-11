019ddcaae857769db98d55ec7e8ba43f24113819
J Seymour <JustJakeSeymour@gmail.com>
2022-12-10 20:25:40 -0700

    write tests for extension story 1

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "Project Show" do
   let!(:news_chic) {recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")}
   let!(:upholstery_tux) {furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")}
   let!(:jay) {Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)}
+  let!(:gretchen) {Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)}
   
   describe "User Story 1" do
     it "shows the project name and material, as well as challenge theme" do
@@ -35,6 +36,22 @@ RSpec.describe "Project Show" do
       #     Material: Lamp Shade
       #   Challenge Theme: Apartment Furnishings
       #   Number of Contestants: 3 )
+    end
+  end
+  
+  describe "Extension Story 1" do
+    it "Average years of experience for contestant by project" do
+      # When I visit a project's show page
+      visit "/projects/#{news_chic.id}"
+      # I see the average years of experience for the contestants that worked on that project
+      expect(page).to have_content("Average Contestant Experience:")
+      expect(page).to have_content("11.5 years")
+
+      # (e.g.    Litfit
+      #     Material: Lamp Shade
+      #   Challenge Theme: Apartment Furnishings
+      #   Number of Contestants: 3
+      #   Average Contestant Experience: 10.25 years)
     end
   end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -30,9 +30,15 @@ RSpec.describe Project, type: :model do
       ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
       ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
       ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
-
+      
       expect(news_chic.contestants_count).to eq(1)
       expect(upholstery_tux.contestants_count).to eq(2)
+    end
+    
+    it "average_years_contestant_experience" do
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+      expect(news_chic.average_years_contestant_experience).to eq(11.5)
     end
   end
 end
