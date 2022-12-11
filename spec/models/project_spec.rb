f9ac54e377db5b6f0b630d552db3272d63fa4cc3
J Seymour <JustJakeSeymour@gmail.com>
2022-12-10 19:20:24 -0700

    instance variable for challenge_theme spec

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,13 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe "instance variables" do
+    it "challenge_theme" do
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+      expect(news_chic.challenge_theme).to eq(recycled_material_challenge.theme)
+    end
+  end
 end
