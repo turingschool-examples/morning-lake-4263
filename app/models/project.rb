ded7be9b5bd301143c2259bf99d0576e4a01c15a
Kerynn Davis <kerynn.davis@gmail.com>
2022-12-10 18:36:53 -0800

    complete us 3

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,8 @@ class Project < ApplicationRecord
   belongs_to :challenge
   has_many :contestant_projects
   has_many :contestants, through: :contestant_projects
+
+  def num_contestants
+    self.contestants.count
+  end 
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,27 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe "num_contestants" do 
+    it 'counts the number of contestants on the project' do 
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+      ContestantProject.create(contestant_id: jay.id, project_id: upholstery_tux.id)
+
+      expect(news_chic.num_contestants).to eq(3)
+      expect(upholstery_tux.num_contestants).to eq(2)
+    end 
+  end 
 end
