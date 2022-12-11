4d6c807844e24007fd5fdfe7a82307ac0d6d7354
Ryan Canton <ryancanton999@gmail.com>
2022-12-11 12:27:29 -0700

    Test: Add test for user_story_2

---
+++ spec/features/contestants/index_spec.rb
@@ -0,0 +1,37 @@
+# As a visitor,
+# When I visit the contestants index page ("/contestants")
+# I see a list of names of all the contestants
+# And under each contestants name I see a list of the projects (names) that they've been on
+
+# (e.g.   Kentaro Kameyama
+#         Projects: Litfit, Rug Tuxedo
+
+#         Jay McCarroll
+#         Projects: LeatherFeather)
+require 'rails_helper'
+
+RSpec.describe 'Contestants index page' do
+  it 'shows all the contestants names and their projects' do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+    upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
+
+    visit "/contestants"
+
+    expect(page).to have_content(gretchen.name)
+    expect(page).to have_content("Projects: #{news_chic.name}, #{upholstery_tux.name}")
+    expect(page).to have_content(kentaro.name)
+    expect(page).to have_content("Projects: #{upholstery_tux.name}, #{boardfit.name}")
+  end
+end
