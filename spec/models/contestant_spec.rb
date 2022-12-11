3ecd022d6401f089b2ff6783c339c5de7537f9bf
Bryan Flanagan <bflanagan78@yahoo.com>
2022-12-11 16:58:22 -0700

    Add contestant_count method to project model for user story 3

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -18,10 +18,8 @@ RSpec.describe Contestant, type: :model do
       it 'returns a list of projects for a contestant' do
         recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
         furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
-
         news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
         boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
-
         upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
         lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
 
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,21 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe 'user story 3' do
+    describe 'contestant_count' do
+      it 'returns count of contestants that worked on a project' do
+        furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+        recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+        news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+        upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+        jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+        gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+        ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+        ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+        ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+        expect(news_chic.contestant_count).to eq(2)
+      end
+    end
+  end
 end
