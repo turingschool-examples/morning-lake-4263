dae3a88b9393cc8d83e4f72ee0f3af7ada45d6f7
Bryan Flanagan <bflanagan78@yahoo.com>
2022-12-11 15:54:32 -0700

    Add contestant index page showing all contestant names

---
+++ spec/features/contestants/index_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Project do
+  describe 'user story 2' do
+    describe 'when I visit /contestants' do
+      it 'shows a list of all contestants and their project name(s)' do
+        recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+        furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+        news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+        boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+        upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+        lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+        jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+        gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+        
+        ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+        ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+        ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+        
+        visit "/contestants"
+        expect(page).to have_content("Contestant Name: Jay McCarroll /n News Chic")
+        expect(page).to have_content("Contestant Name: Gretchen Jones /n News Chic, Upholstery Tux")
+      end
+    end
+  end
+end
--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -1,24 +1,21 @@
 require 'rails_helper'
 
-# As a visitor,
-# When I visit a project's show page ("/projects/:id"),
-# I see that project's name and material
-# And I also see the theme of the challenge that this project belongs to.
 RSpec.describe Project do
-  describe 'when I visit /projects/:id' do
-    it 'sees the project name and materials and the theme of the project challenge' do
-      recycled_material_challenge = Challenge.create!(theme: "Recycled Material", project_budget: 1000)
-      furniture_challenge = Challenge.create!(theme: "Apartment Furnishings", project_budget: 1000)
-      news_chic = recycled_material_challenge.projects.create!(name: "News Chic", material: "Newspaper")
-      boardfit = recycled_material_challenge.projects.create!(name: "Boardfit", material: "Cardboard Boxes")
-      upholstery_tux = furniture_challenge.projects.create!(name: "Upholstery Tuxedo", material: "Couch")
-      lit_fit = furniture_challenge.projects.create!(name: "Litfit", material: "Lamp")
+  describe 'user story 1' do
+    describe 'when I visit /projects/:id' do
+      it 'shows the project name and materials and the theme of the project challenge' do
+        recycled_material_challenge = Challenge.create!(theme: "Recycled Material", project_budget: 1000)
+        furniture_challenge = Challenge.create!(theme: "Apartment Furnishings", project_budget: 1000)
+        news_chic = recycled_material_challenge.projects.create!(name: "News Chic", material: "Newspaper")
+        boardfit = recycled_material_challenge.projects.create!(name: "Boardfit", material: "Cardboard Boxes")
+        upholstery_tux = furniture_challenge.projects.create!(name: "Upholstery Tuxedo", material: "Couch")
+        lit_fit = furniture_challenge.projects.create!(name: "Litfit", material: "Lamp")
 
-      visit "/projects/#{lit_fit.id}"
-      expect(page).to have_content("Challenge Theme: #{furniture_challenge.theme}")
-      expect(page).to have_content("Material: #{lit_fit.material}")
-      expect(page).to have_content("Challenge Name: #{lit_fit.name}")
-
+        visit "/projects/#{lit_fit.id}"
+        expect(page).to have_content("Challenge Theme: #{furniture_challenge.theme}")
+        expect(page).to have_content("Material: #{lit_fit.material}")
+        expect(page).to have_content("Challenge Name: #{lit_fit.name}")
+      end
     end
   end
 end
