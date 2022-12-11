e2f6288bdc3954e57e2431ba8b2efc54fef63046
Anhnhi Tran <anhtran811@gmail.com>
2022-12-11 14:57:32 -0700

    User story 2 complete

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -24,5 +24,29 @@ RSpec.describe 'Contestants Index page' do
       expect(page).to have_content(gretchen.name)
       expect(page).to have_content(kentaro.name)
     end
+
+    it 'can show the list of project names per contestant' do
+      jay = Contestant.create!(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create!(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+    
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+      upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+      lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+  
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: lit_fit.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+      
+      visit '/contestants'
+
+      expect(page).to have_content("Project: #{news_chic.name}")
+      expect(page).to have_content("Project: #{lit_fit.name}")
+      expect(page).to have_content("Project: #{upholstery_tux.name}")
+      expect(page).to_not have_content("Project: #{boardfit.name}")
+    end
   end
 end
