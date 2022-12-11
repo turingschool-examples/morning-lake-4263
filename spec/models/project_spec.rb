458bb194e037e9c042ca67360ef27b755cf3b867
J Seymour <JustJakeSeymour@gmail.com>
2022-12-10 20:19:00 -0700

    100 pct coverage with models and features

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -14,15 +14,25 @@ RSpec.describe Project, type: :model do
   end
 
   describe "instance variables" do
-    it "challenge_theme" do
-      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    let!(:recycled_material_challenge) {Challenge.create(theme: "Recycled Material", project_budget: 1000)}
+    let!(:furniture_challenge) {Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)}
+    let!(:news_chic) {recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")}
+    let!(:upholstery_tux) {furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")}
+    let!(:jay) {Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)}
+    let!(:gretchen) {Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)}
+    let!(:kentaro) {Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)}
 
+    it "challenge_theme" do
       expect(news_chic.challenge_theme).to eq(news_chic.challenge.theme)
     end
 
     it "contestants_count" do
-      
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+
+      expect(news_chic.contestants_count).to eq(1)
+      expect(upholstery_tux.contestants_count).to eq(2)
     end
   end
 end
