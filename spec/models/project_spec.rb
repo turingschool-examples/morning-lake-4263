9b132f9e35b04bc8bdb5ce125c039d4b1bd4c116
Kelsie Porter <krporter22@gmail.com>
2022-12-11 20:27:33 -0700

    Add model test for #contestant_count

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,29 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe '#contestant_count' do 
+    it 'returns the number of contestants on a specific project' do
+      furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+      
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+      erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+      news_chic.contestants << jay
+      news_chic.contestants << gretchen
+      news_chic.contestants << kentaro
+
+      upholstery_tux.contestants << gretchen
+      upholstery_tux.contestants << kentaro
+
+      expect(news_chic.contestant_count).to eq(3)
+      expect(upholstery_tux.contestant_count).to eq(2)
+    end
+  end
 end
