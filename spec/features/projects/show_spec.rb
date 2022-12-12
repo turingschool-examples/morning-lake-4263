cc15a26dcf4e0e94c8c1b235c3cd8bfc0af83195
Kelsie Porter <krporter22@gmail.com>
2022-12-11 20:03:56 -0700

    feat: Add contestant count to projects show page

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -7,9 +7,14 @@ RSpec.describe 'projects show page', type: :feature do
 
     @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
     @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
-
     @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
     @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
   end
   it 'displays the specific projects name and material' do
     visit "/projects/#{@news_chic.id}"
@@ -21,13 +26,33 @@ RSpec.describe 'projects show page', type: :feature do
     visit "/projects/#{@boardfit.id}"
 
     expect(page).to have_content("Boardfit")
-    expect(page).to have_content("Newspaper")
-    expect(page).to_not have_content("Boardfit")
+    expect(page).to have_content("Cardboard Boxes")
+    expect(page).to_not have_content("News Chic")
   end
 
   it 'displays the theme of the challenge that the project belongs to' do
     visit "/projects/#{@news_chic.id}"
-    save_and_open_page
+
     expect(page).to have_content("Recycled Material")
+  end
+
+  it 'displays the number on contestants on the specific project' do
+    @news_chic.contestants << @jay
+    @news_chic.contestants << @gretchen
+    @news_chic.contestants << @erin
+    
+    visit "/projects/#{@news_chic.id}"
+    save_and_open_page
+    expect(page).to have_content("Number of Contestants: 3")
+
+    @boardfit.contestants << @jay
+    @boardfit.contestants << @gretchen
+    @boardfit.contestants << @erin
+    @boardfit.contestants << @kentaro
+
+    visit "/projects/#{@boardfit.id}"
+    save_and_open_page
+
+    expect(page).to have_content("Number of Contestants: 4")
   end
 end
