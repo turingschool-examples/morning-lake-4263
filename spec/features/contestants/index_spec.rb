75deffa0a4793ada3f7ddb534bbb2995f6364b17
Joe King <j.king92@gmail.com>
2022-12-12 15:55:51 -0700

    fix linting

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'contestants index' do
   it 'lists all contestant names and the prjects they have been on' do
-    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
-    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
-    upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
-    lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
-    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
-    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
-    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    recycled_material_challenge = Challenge.create(theme: 'Recycled Material', project_budget: 1000)
+    furniture_challenge = Challenge.create(theme: 'Apartment Furnishings', project_budget: 1000)
+    news_chic = recycled_material_challenge.projects.create(name: 'News Chic', material: 'Newspaper')
+    boardfit = recycled_material_challenge.projects.create(name: 'Boardfit', material: 'Cardboard Boxes')
+    upholstery_tux = furniture_challenge.projects.create(name: 'Upholstery Tuxedo', material: 'Couch')
+    lit_fit = furniture_challenge.projects.create(name: 'Litfit', material: 'Lamp')
+    jay = Contestant.create(name: 'Jay McCarroll', age: 40, hometown: 'LA', years_of_experience: 13)
+    gretchen = Contestant.create(name: 'Gretchen Jones', age: 36, hometown: 'NYC', years_of_experience: 12)
+    kentaro = Contestant.create(name: 'Kentaro Kameyama', age: 30, hometown: 'Boston', years_of_experience: 8)
     ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
     ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
     ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
@@ -19,21 +19,21 @@ RSpec.describe 'contestants index' do
 
     visit '/contestants'
 
-    within("div##{ jay.id }") do
+    within("div##{jay.id}") do
       expect(page).to have_content(jay.name)
       jay.projects.each do |project|
         expect(page).to have_content(project.name)
       end
-    end    
+    end
 
-    within("div##{ gretchen.id }") do
+    within("div##{gretchen.id}") do
       expect(page).to have_content(gretchen.name)
       gretchen.projects.each do |project|
         expect(page).to have_content(project.name)
       end
-    end    
-    
-    within("div##{ kentaro.id }") do
+    end
+
+    within("div##{kentaro.id}") do
       expect(page).to have_content(kentaro.name)
       kentaro.projects.each do |project|
         expect(page).to have_content(project.name)
--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'projects show' do
   it 'shows the projects name, material and theme of challenge' do
-    recycled_material_challenge = Challenge.create!(theme: "Recycled Material", project_budget: 1000)
-    furniture_challenge = Challenge.create!(theme: "Apartment Furnishings", project_budget: 1000)
-    news_chic = recycled_material_challenge.projects.create!(name: "News Chic", material: "Newspaper")
-    boardfit = recycled_material_challenge.projects.create!(name: "Boardfit", material: "Cardboard Boxes")
+    recycled_material_challenge = Challenge.create!(theme: 'Recycled Material', project_budget: 1000)
+    furniture_challenge = Challenge.create!(theme: 'Apartment Furnishings', project_budget: 1000)
+    news_chic = recycled_material_challenge.projects.create!(name: 'News Chic', material: 'Newspaper')
+    boardfit = recycled_material_challenge.projects.create!(name: 'Boardfit', material: 'Cardboard Boxes')
 
-    visit "/projects/#{ news_chic.id }"
+    visit "/projects/#{news_chic.id}"
 
     expect(page).to have_content(news_chic.name)
     expect(page).to have_content(news_chic.material)
@@ -15,68 +15,58 @@ RSpec.describe 'projects show' do
   end
 
   it 'shows a count of the number of contestants on the project' do
-    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
-    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
-    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
-    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    recycled_material_challenge = Challenge.create(theme: 'Recycled Material', project_budget: 1000)
+    boardfit = recycled_material_challenge.projects.create(name: 'Boardfit', material: 'Cardboard Boxes')
+    jay = Contestant.create(name: 'Jay McCarroll', age: 40, hometown: 'LA', years_of_experience: 13)
+    gretchen = Contestant.create(name: 'Gretchen Jones', age: 36, hometown: 'NYC', years_of_experience: 12)
+    kentaro = Contestant.create(name: 'Kentaro Kameyama', age: 30, hometown: 'Boston', years_of_experience: 8)
     ContestantProject.create(contestant_id: jay.id, project_id: boardfit.id)
     ContestantProject.create(contestant_id: gretchen.id, project_id: boardfit.id)
     ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
 
-    visit "/projects/#{ boardfit.id }"
+    visit "/projects/#{boardfit.id}"
 
-    expect(page).to have_content("Number of Contestants: 3")
+    expect(page).to have_content('Number of Contestants: 3')
   end
 
   it 'shows the average years of experience for contestants on the project' do
-    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
-    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
-    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
-    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    recycled_material_challenge = Challenge.create(theme: 'Recycled Material', project_budget: 1000)
+    boardfit = recycled_material_challenge.projects.create(name: 'Boardfit', material: 'Cardboard Boxes')
+    jay = Contestant.create(name: 'Jay McCarroll', age: 40, hometown: 'LA', years_of_experience: 13)
+    gretchen = Contestant.create(name: 'Gretchen Jones', age: 36, hometown: 'NYC', years_of_experience: 12)
+    kentaro = Contestant.create(name: 'Kentaro Kameyama', age: 30, hometown: 'Boston', years_of_experience: 8)
     ContestantProject.create(contestant_id: jay.id, project_id: boardfit.id)
     ContestantProject.create(contestant_id: gretchen.id, project_id: boardfit.id)
     ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
 
-    visit "/projects/#{ boardfit.id }"
+    visit "/projects/#{boardfit.id}"
 
-    expect(page).to have_content("Average Contestant Experience: 11.0 years")
+    expect(page).to have_content('Average Contestant Experience: 11.0 years')
   end
 
   it 'has a field for adding a contestant to project using contestants id' do
-    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
-    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
-    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
-    ken = Contestant.create(name: "Ken Kam", age: 30, hometown: "Boston", years_of_experience: 8)
+    recycled_material_challenge = Challenge.create(theme: 'Recycled Material', project_budget: 1000)
+    boardfit = recycled_material_challenge.projects.create(name: 'Boardfit', material: 'Cardboard Boxes')
+    jay = Contestant.create(name: 'Jay McCarroll', age: 40, hometown: 'LA', years_of_experience: 13)
+    gretchen = Contestant.create(name: 'Gretchen Jones', age: 36, hometown: 'NYC', years_of_experience: 12)
+    ken = Contestant.create(name: 'Ken Kam', age: 30, hometown: 'Boston', years_of_experience: 8)
     ContestantProject.create(contestant_id: jay.id, project_id: boardfit.id)
     ContestantProject.create(contestant_id: gretchen.id, project_id: boardfit.id)
 
-    visit "/projects/#{ boardfit.id }"
+    visit "/projects/#{boardfit.id}"
 
-    expect(page).to have_content("Number of Contestants: 2")
+    expect(page).to have_content('Number of Contestants: 2')
 
     fill_in('ContestantID', with: ken.id)
     click_button('Add Contestant To Project')
 
-    expect(current_path).to eq("/projects/#{ boardfit.id }")
-    expect(page).to have_content("Number of Contestants: 3")
+    expect(current_path).to eq("/projects/#{boardfit.id}")
+    expect(page).to have_content('Number of Contestants: 3')
 
     visit '/contestants'
 
-    within("div##{ ken.id }") do
+    within("div##{ken.id}") do
       expect(page).to have_content(boardfit.name)
     end
   end
 end
-
-# As a visitor,
-# When I visit a project's show page
-# I see a form to add a contestant to this project
-# When I fill out a field with an existing contestants id
-# And hit "Add Contestant To Project"
-# I'm taken back to the project's show page
-# And I see that the number of contestants has increased by 1
-# And when I visit the contestants index page
-# I see that project listed under that contestant's name
