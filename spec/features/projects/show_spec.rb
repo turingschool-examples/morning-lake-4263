153d39fe18a57103543f22b30149d14c6940b63f
Joe King <j.king92@gmail.com>
2022-12-12 14:40:27 -0700

    Add gem "net-http" and add project show page feature and testing

---
+++ spec/features/projects/show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'projects show' do
+  it 'shows the projects name, material and theme of challenge' do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+    visit "/projects/#{ news_chic.id }"
+    save_and_open_page
+    expect(page).to have_content(news_chic.name)
+    expect(page).to have_content(news_chic.material)
+    expect(page).to have_content(recycled_material_challenge.theme)
+  end
+end
+
+# As a visitor,
+# When I visit a project's show page ("/projects/:id"),
+# I see that project's name and material
+# And I also see the theme of the challenge that this project belongs to.
+# (e.g.    Litfit
+#     Material: Lamp Shade
+#   Challenge Theme: Apartment Furnishings)
