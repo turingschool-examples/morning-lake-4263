27e1a8ca1b08bf9a5f66678df84546d881f8fb3f
cemccabe <46434166+cemccabe@users.noreply.github.com>
2022-12-13 15:25:20 -0700

    Update Project model relationships for many-to-many; Pass respective test

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,5 @@ class Project < ApplicationRecord
   validates_presence_of :name, :material
   belongs_to :challenge
   has_many :contestant_projects
+  has_many :contestants, through: :contestant_projects
 end
