f8d445f31c66a411b42fe140b52e150b78ad631a
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-12-12 19:45:33 -0600

    add contestants and contestant_projects to projects show spec before each block

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -9,7 +9,15 @@ require 'rails_helper'
 #     Material: Lamp Shade
 #   Challenge Theme: Apartment Furnishings)
 
+# User Story 3 of 3
+# As a visitor,
+# When I visit a project's show page
+# I see a count of the number of contestants on this project
 
+# (e.g.    Litfit
+#     Material: Lamp Shade
+#   Challenge Theme: Apartment Furnishings
+#   Number of Contestants: 3 )
 RSpec.describe 'projects show page' do
   describe 'As a visitor' do
     describe 'when I visit a projects show page "/projects/:id"' do
@@ -22,6 +30,19 @@ RSpec.describe 'projects show page' do
 
         @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
         @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+        @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+        @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+        @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+        @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+
+        ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+        ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+        ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+        ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+        ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+        ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
         visit "/projects/#{@lit_fit.id}"
       end
       it 'I see that projects name and material' do
