a107e32331896dd1a3662d1b50e4a1864829be3b
Drew Layton <dlayton66@gmail.com>
2022-12-12 21:11:24 -0700

    feat: Contestants Index Page shows contestant and associated projects

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -14,24 +14,24 @@ RSpec.describe "Contestants Index Page" do
       visit "/contestants"
 
       expect(page).to have_content(@jay.name)
-      within("#projects-#{@jay.id}") do
+      within("#contestant-#{@jay.id}") do
         expect(page).to have_content("News Chic")
       end
 
       expect(page).to have_content(@gretchen.name)
-      within("#projects-#{@gretchen.id}") do
+      within("#contestant-#{@gretchen.id}") do
         expect(page).to have_content("News Chic")
         expect(page).to have_content("Upholstery Tuxedo")
       end
 
       expect(page).to have_content(@kentaro.name)
-      within("#projects-#{@kentaro.id}") do
+      within("#contestant-#{@kentaro.id}") do
         expect(page).to have_content("Upholstery Tuxedo")
         expect(page).to have_content("Boardfit")
       end
 
       expect(page).to have_content(@erin.name)
-      within("#projects-#{@erin.id}") do
+      within("#contestant-#{@erin.id}") do
         expect(page).to have_content("Boardfit")
       end
     end
--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-
+require 'test_helper'
 
 RSpec.describe Contestant, type: :model do
   describe "validations" do
@@ -12,5 +12,16 @@ RSpec.describe Contestant, type: :model do
   describe "relationships" do
     it {should have_many :contestant_projects}
     it {should have_many(:projects).through(:contestant_projects)}
+  end
+
+  describe "#list_projects" do
+    it 'puts all contestant projects into a comma-separated list' do
+      seed_challenges
+      seed_projects
+      seed_contestants
+      seed_contestant_projects
+
+      expect(@gretchen.list_projects).to eq("News Chic, Upholstery Tuxedo")
+    end
   end
 end
