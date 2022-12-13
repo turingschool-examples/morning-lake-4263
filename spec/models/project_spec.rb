a8215048514e1a2e359167695beceb1c96b4bf13
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-12-13 11:48:35 -0800

    Add model test for contestant count

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  it 'can count how many contestants it has' do
+    @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+
+    expect(@news_chic.contestant_count).to eq(2)
+  end
 end
