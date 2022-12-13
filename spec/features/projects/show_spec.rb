df7ce26a4dcf58482dcf56cdf4bef8203a67b1c3
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-12-12 16:16:08 -0800

    Test: projects show page

---
+++ spec/features/projects/show_spec.rb
@@ -0,0 +1,28 @@
+# As a visitor,
+# When I visit a project's show page ("/projects/:id"),
+# I see that project's name and material
+# And I also see the theme of the challenge that this project belongs to.
+# (e.g.    Litfit
+#     Material: Lamp Shade
+#   Challenge Theme: Apartment Furnishings)
+
+require 'rails_helper'
+
+RSpec.describe 'project show page' do
+  it 'displays projects name and material, and them of challenge' do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+    upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+    visit "/projects/#{news_chic.id}"
+
+    expect(page).to have_content("News Chic")
+    expect(page).to have_content("Material: Newspaper")
+    expect(page).to have_content("Challenge Theme: Recycled Material")
+    expect(page).to have_no_content("Apartment Furnishings")
+    expect(page).to have_no_content("Litfit")
+  end
+end
